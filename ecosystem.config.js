4cc9cbfa2be47ce2adc501695ba11add35830c21
Juan Jaramillo <info@juanjaramillo.tech>
2026-01-16 20:11:50 -0500

    style: format ecosystem.config.js
    - Update ecosystem.config.js to use double quotes for consistency with project style.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: 'topfinanzas-mx-next',
-      script: 'npm',
-      args: 'start',
-      cwd: '/var/www/html/topfinanzas-mx-next',
+      name: "topfinanzas-mx-next",
+      script: "npm",
+      args: "start",
+      cwd: "/var/www/html/topfinanzas-mx-next",
       env: {
         PORT: 3030,
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
     },
   ],
 };
